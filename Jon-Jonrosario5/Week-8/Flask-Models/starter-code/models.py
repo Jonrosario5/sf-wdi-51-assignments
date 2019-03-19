4306634c95099148fcb485f57c513ce77a536786
Jonrosario5 <Jonathan.Rosario550@gmail.com>
2019-03-18 17:11:44 -0700

    Finalized the Flask Models Comments Models

---
+++ Jon-Jonrosario5/Week-8/Flask-Models/starter-code/models.py
@@ -0,0 +1,48 @@
+import datetime
+# peewee is a small and simple ORM, like mongoose but simpler
+from peewee import *
+
+# We use the built in SqliteDatabase() function from peewee 
+# to save a reference to a DB file to a DATABASE variable
+DATABASE = SqliteDatabase('breddit.db')
+
+# Our Sub model is pretty simple, just a name and description for now
+class Sub(Model):
+    timestamp = DateTimeField(default=datetime.datetime.now)
+    name = CharField()
+    description = TextField()
+
+    class Meta:
+        database = DATABASE
+        order_by =('-timestamp')
+
+class Post(Model):
+    timestamp = DateTimeField(default=datetime.datetime.now)
+    user = CharField()
+    title = CharField()
+    text = TextField()
+    #relate the Post Model to the Sub Model
+    #Will allow you to call post.sub to tie them together.
+    sub = ForeignKeyField(Sub, backref='posts')
+
+    class Meta:
+        database = DATABASE
+        order_by =('-timestamp')
+
+class Comment(Model):
+    timestamp = DateTimeField(default=datetime.datetime.now)
+    user = CharField()
+    text = TextField()
+    post = ForeignKeyField(Post, backref='comments')
+
+    class Meta:
+        database = DATABASE
+        order_by = ('-timestamp')
+    
+
+# We initialize a connection to the DATABASE, create a table for the Sub model, 
+# and close the connection
+def initialize():
+    DATABASE.connect()
+    DATABASE.create_tables([Sub, Post], safe=True)
+    DATABASE.close()
